6277c626c50073a062c9443e5ab9f97711023b19
htlin <asnif24@gmail.com>
2016-09-21 15:35:08 +0800

    index

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,23 @@
-function StoreController(){
-	alert('Welcome!!');
-}
+(function(){
+	var app=angular.module('store',[]);
+	app.controller('StoreController', function(){
+		this.products=gems;
+	});
+	
+	var gems=[
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: '...',
+		canPurchase: true,
+		soldOut: false,
+	},
+	{
+		name: 'Pentagonal Gem',
+		price: 5.95,
+		description: '...',
+		canPurchase: true,
+		soldOut: true,
+	}
+	]
+})();
